9d518c1ee6acdb4aa03e9a2a1f80a38f825a9276
Noah Hdez <legolasbat@gmail.com>
2025-12-03 20:13:33 +0000

    Update texture.cpp

--- src/texture.cpp
+++ src/texture.cpp
@@ -44,6 +44,31 @@ bool Texture::LoadTexture() {
 	return true;
 }
 
+bool Texture::LoadTextureA() {
+	unsigned char* textData = stbi_load(fileLocation, &width, &height, &bitDepth, 0);
+	if (!textData) {
+		printf("Failed to find: %s\n", fileLocation);
+		return false;
+	}
+
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textData);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	stbi_image_free(textData);
+
+	return true;
+}
+
 void Texture::GetTextureID(GLuint* textureID) const {
 	*textureID = this->textureID;
 }
